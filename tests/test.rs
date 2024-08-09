8558b6b7449722ed4a85aa25c29be6e76eaa8ccd
KitaSan <info@kitasan.io>
2024-08-09 08:52:46 +0700

    add LICENSE

--- tests/test.rs
+++ tests/test.rs
@@ -1,4 +1,16 @@
-// ファイル: tests/test.rs
+// Copyright 2024 KitaSan <info@kitasan.io>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 use zen_han_converter::{zen_to_han, han_to_zen};
 
